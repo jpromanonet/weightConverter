b326bbba2e6cec9fa2232db6c9f2f8c4a7562329
Juan P. Romano <35672464+jpromanonet@users.noreply.github.com>
2019-08-02 09:36:49 -0300

    More weight units added

--- weightConverter.js
+++ weightConverter.js
@@ -5,4 +5,6 @@ document.getElementById("lbsInput").addEventListener("input", function(e) {
   document.getElementById("gramsOutput").innerHTML = lbs / 0.0022046 + " gr";
   document.getElementById("kgOutput").innerHTML = lbs / 2.2046 + " Kg";
   document.getElementById("ozOutput").innerHTML = lbs * 16 + " Oz";
+  document.getElementById("stOutput").innerHTML = lbs / 14 + " st";
+  document.getElementById("tnOutput").innerHTML = lbs /  2204.623 + " tn";
 });
